57e9d453c5911401f86e3ea40725138bd8f12c34
Jin Tao <taojin1992@users.noreply.github.com>
2018-11-15 12:34:08 -0800

    Add files via upload

--- src/face-recognition-TJ.py
+++ src/face-recognition-TJ.py
@@ -110,7 +110,7 @@ def recognize_face(unknown_face_embedding, database):
 			min_dist = dist
 			identity = name
 	# set the threshold
-	if(min_dist > 0.9): 
+	if(min_dist > 1.1): 
 		similar = identity
 		identity = "unknown but is most similar to " + similar
 	return identity,min_dist
@@ -198,7 +198,7 @@ if __name__ == '__main__':
 #	simplify('/Users/jintao01/Documents/Arm-Buddies/unknown_pics/5.jpg','/Users/jintao01/Documents/Arm-Buddies/5.jpg')
 	database = initialize('/Users/jintao01/Documents/facematch/20180204-160909', '/Users/jintao01/Documents/Arm-Buddies', 160, 44, 1.0)
 	# print(database)
-	person_file = ['/Users/jintao01/Documents/Arm-Buddies/unknown_pics/3.jpg']
+	person_file = ['/Users/jintao01/Documents/Arm-Buddies/unknown_pics/9.jpeg']
 	unknown_face_embedding = get_single_embedding(person_file, '/Users/jintao01/Documents/facematch/20180204-160909', 160, 44, 1.0)
 	print(recognize_face(unknown_face_embedding, database))
 
@@ -206,3 +206,4 @@ if __name__ == '__main__':
 # for better result, train from scatch using customer's dataset
 # for conveinience, using the pretrained model means no need to retrain the model when new faces are added
 
+# add the tag to the detected face if needed
